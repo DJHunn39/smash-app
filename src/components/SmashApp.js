bd0cedafd875b5fc88cddfbe2bbe5ca80640b2f5
DJHunn39 <dannyjohnhunn@yahoo.co.uk>
2018-12-29 15:37:41 +0000

    Show all the fighters on the page

--- src/components/SmashApp.js
+++ src/components/SmashApp.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 
 const SmashApp = ({ fighterData }) => {
-
-  return(
-    <div>
-      <p>{fighterData[0].name}</p>
-      <img src={fighterData[0].icon} alt={fighterData[0].name} />
-    <p>Weight ranking: {fighterData[0].ranking.weight}</p>
-    </div>
-  )
+  const fighterTable = fighterData.map(fighter => {
+    return(
+  <div className='fighter-flexbox'>
+      <img src={fighter.picture} alt={fighter.name} height={50} width={50}/>
+      <span className='fighter-name fighter-text'>{fighter.name}</span>
+      <span className='fighter-text'>Weight ranking: {fighter.ranking.weight}</span>
+  </div>
+)
+});
+return(
+  <Fragment>
+    {fighterTable}
+  </Fragment>
+)
 }
 
 export default SmashApp;
